2d827777c0f8b719d79d28ee63149971418fdfc3
masterfapper72 <leonardo.mazzoleni1@gmail.com>
2023-01-07 12:19:55 +0100

    add better folder navigation and started handle file upload

--- app/Http/Livewire/files/index.php
+++ app/Http/Livewire/files/index.php
@@ -8,23 +8,30 @@ use Livewire\Component;
 
 class Index extends Component
 {
-    public $folders;
-    public $files = [];
-    public $depth = 0;
+    public $folders;    // List of folders
+    public Folder $folder;  // Data of specific clicked folder
+    public $files = []; // List of files in folder
+    public $depth = 0;  // Depth level of clicked folder
+    public $showFilesIfNull;    // Don't show files in folder if $folder value is null
+    public $file;   // Uploaded file data
 
     public function mount() {
         
         $this->getFolders();
     }
 
-    public function showFiles() {
+    // Show all files and folders
+    public function showFiles(Folder $folder) {
         $this->depth += 1;  // show file/folders contained in clicked folder
+        $this->folder = $folder;
+        $this->showFilesIfNull = true;
 
         $this->getFiles();
 
         $this->getFolders();
     }
 
+    // Get all folders at the specified depth level
     private function getFolders() {
 
         $this->folders = Folder::select()
@@ -32,11 +39,35 @@ class Index extends Component
             ->get();
     }
 
+    // Get all files in selected folder
     private function getFiles() {
 
         $this->files = File::select()
-            ->where('folder_id', 1)
+            ->where('folder_id', $this->folder->id)
             ->get();
+    }
+
+    // Navigate back to previous folders
+    public function goBack() {
+        $this->depth -= 1;
+        $folderCheck = Folder::select()
+            ->where('id', "<", $this->folder->id)
+            ->orderBy('id')
+            ->first();
+        
+        if (!is_null($folderCheck)) {
+            $this->folder = $folderCheck;
+            $this->getFiles();
+        } else {
+            $this->showFilesIfNull = false;
+        }
+
+        $this->getFolders();
+    }
+
+    // Handle new file upload
+    public function upload() {
+        // TO-DO
     }
 
     public function render()
--- resources/views/livewire/files/index.blade.php
+++ resources/views/livewire/files/index.blade.php
@@ -4,20 +4,41 @@ border-zinc-900 shadow-2xl mt-20">
         <div>
             <h2 class="text-red-800 font-semibold text-3xl shadow-text">YOUR FILES</h2>
         </div>
-        <div class="w-full p-5 bg-red-500 rounded-lg overflow-y-auto h-screen">
+        <div class="w-full p-5 bg-red-800 rounded-lg overflow-y-auto h-screen">
             <div class="pb-10 @if ($depth == 0) hidden @endif">
-                <i class="fa-solid fa-arrow-left text-4xl cursor-pointer"></i>
+                <i wire:click="goBack" class="fa-solid fa-arrow-left text-4xl cursor-pointer"></i>
             </div>
             @foreach ($folders as $folder)
-                <div class="flex flex-col space-y-2 w-fit">
-                    <a wire:click="showFiles"><i class="fa-solid fa-folder cursor-pointer text-8xl"></i></a>
-                    <span class="font-semibold text-2xl text-center">{{$folder->name}}</span>
+                <div class="flex items-center space-x-10">
+                    <div class="flex flex-col space-y-2 w-fit">
+                        <a wire:click="showFiles({{$folder}})">
+                            <i class="fa-solid fa-folder cursor-pointer text-8xl"></i>
+                        </a>
+                        <span class="font-semibold text-2xl text-center">{{$folder->name}}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-folder-plus cursor-pointer text-6xl"></i>
+                    </div>
                 </div>
             @endforeach
             @foreach ($files as $file)
-                <div class="flex flex-col space-y-2 w-fit">
-                    <a><i class="fa-solid fa-file cursor-pointer text-8xl"></i></a>
-                    <span class="font-semibold text-2xl text-center">{{$file->realName}}</span>
+                <div class="flex items-center space-x-10 @if (!$showFilesIfNull) hidden @endif">
+                    <div class="flex flex-col space-y-2 w-fit">
+                        <a><i class="fa-solid fa-file cursor-pointer text-8xl"></i></a>
+                        <span class="font-semibold text-2xl text-center">{{$file->realName}}</span>
+                    </div>
+                    <div class="flex space-x-5">
+                        <i class="fa-solid fa-folder-plus cursor-pointer text-6xl"></i>
+                        <form id="uploadForm" wire:submit.prevent="upload" class="hidden">
+                            <input 
+                            id="file" 
+                            type="file" 
+                            wire:model="file" 
+                            onchange="document.getElementById('uploadForm').submit()">
+                        </form>
+                        <i onclick="document.getElementById('file').click()" 
+                        class="fa-solid fa-file-circle-plus cursor-pointer text-6xl"></i>   
+                    </div>
                 </div>
             @endforeach
         </div>
